a3042e473a60c4d8207d1deeaf1b2891a08c2b4c
Mauro Leoci <xcibe95x@gmail.com>
2022-07-13 12:52:53 +0200

    Update imports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 // Import all the components you want to render here
+
+// Introduction Tutorial
 import "./components/Introductive/SimpleGreeting";
 import "./components/Introductive/SimpleInput";
 import "./components/Introductive/SimpleToDo";
+
+// Working With Lists Tutorial
 import "./components/ItemsListing/MapDirective";
 import "./components/ItemsListing/FilteredList";
 import "./components/ItemsListing/RenderableArray";
+import "./components/ItemsListing/RangeDirective";
+
+// Custom
 import "./components/UserCard.js";
